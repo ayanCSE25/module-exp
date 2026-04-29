9c08030f53c3071cfc44f8fee7bf2f4f63e8dadf
harshal <harshalh.cse25@sbjit.edu.in>
2026-04-29 22:58:57 +0530

    add quiz question data

--- script.js
+++ script.js
@@ -4,3 +4,44 @@ question:"Which language is used for web development?",
 options:["HTML","Python","C","Java"],
 answer:0
 },
+{
+question:"Which tag creates a hyperlink?",
+options:["img","a","link","href"],
+answer:1
+},
+{
+question:"CSS stands for?",
+options:[
+"Creative Style Sheets",
+"Cascading Style Sheets",
+"Colorful Style Sheets",
+"Computer Style Sheets"
+],
+answer:1
+},
+{
+question:"JavaScript is used for?",
+options:[
+"Styling",
+"Database",
+"Interactivity",
+"Hardware"
+],
+answer:2
+},
+{
+question:"Which is frontend language?",
+options:[
+"HTML",
+"SQL",
+"Python",
+"PHP"
+],
+answer:0
+}
+];
+
+let current=0;
+let score=0;
+let time=15;
+let timer;
